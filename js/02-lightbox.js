212355808342f01cdbd3c29d3c7786414480ba2e
Anton Bershak <anton.bershak@gmail.com>
2022-01-28 00:33:45 +0200

    Add task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const GalleryItemMarkup = creatGalleryCardMarkup(galleryItems);
+
+function creatGalleryCardMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+    }).join('');
+};
+
+galleryEl.insertAdjacentHTML('beforeend', GalleryItemMarkup);
+new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250 } );
